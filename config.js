1d7fdb3cfd2530fddb58b72eb863b784c8aa9a90
Goc-yan <632114989@qq.com>
2019-04-11 23:27:13 +0800

    remove config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const AccessKeyId = 'LTAIHExHtCOnIlMW'
-const AccessKeySecret = 'eooozhRB7EOU0GECxcQ1c7PHCNUoeH'
-const RecordId = '16995417818096640'
-
-module.exports = {
-    AccessKeyId,
-    AccessKeySecret,
-    RecordId
-}
