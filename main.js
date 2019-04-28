e983d063f601479fdc03e757f410e261bbe1b384
Goc-yan <632114989@qq.com>
2019-04-28 20:02:21 +0800

    optimization

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const { LOOP_TIME } = require('./config')
-const { AccessKeyId, AccessKeySecret } = require('./config/user')
+const { AccessKeyId, AccessKeySecret, RecordId } = require('./config/user')
 
 const AliCloudClient = require('./aliCloudClient')
-const { getIP } = require('./utils')
+const { getCurrentIP, log, getLastLog } = require('./utils')
 
 let main = async function () {
+
+    let perIPRecord = await getLastLog()
+    let perIP = perIPRecord ? perIPRecord.split(' => ')[1] : ''
+    let currentIP = await getCurrentIP()
+    
+    log(currentIP)
+
+
+    return 
+    // 若IP相同
+    if(perIP === currentIP) return 
+    
 
     // 更新 DomainRecord
     let UpdateDomainRecord = function (RecordId, IP) {
@@ -17,46 +28,18 @@ let main = async function () {
             RecordId,
         }
         return aliCloudClient.get(options).then(res => {
-            console.log('update success')
+            log(currentIP + ' => update success')
         })
-    }
-
-    // 获取记录
-    let getRecord = function (alcc) {
-        return alcc.get({
-            Action: 'DescribeDomainRecords',
-            DomainName: 'gocyan.cn'
-        })
-    }
-
-    // 轮询方法
-    let loop = async function () {
-
-        let localIP = await getIP()
-
-        if (localIP !== IP) {
-            console.log('本地 IP 改变, 更新域名记录值')
-            await UpdateDomainRecord(RecordId, localIP)
-            console.log('域名记录值更新成功')
-        }
-        setTimeout(loop, LOOP_TIME * 1000)
     }
 
     let aliCloudClient = new AliCloudClient({
         AccessKeyId,
         AccessKeySecret,
     })
-  
-    // 获取 aliCloud 的记录值 ( IP, RecordId )
-    let RecDomainRecordsord = await getRecord(aliCloudClient)
-    let Record = RecDomainRecordsord.DomainRecords.Record[0]
-    let IP = Record.Value
-    let RecordId = Record.RecordId
 
-    console.log('已获取 IP & RecordId')
+    // 更新域映射IP
+    UpdateDomainRecord(RecordId, currentIP)
 
-    // 轮询本地 IP
-    loop()
 }
 
 main()
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,7 @@
 const http = require('http')
 const crypto = require("crypto")
+const fs = require('fs')
+const path = require('path')
 
 const httpGet = function (url, options = {}, encode = false) {
 
@@ -13,7 +15,9 @@ const httpGet = function (url, options = {}, encode = false) {
 
         http.get(url, res => {
 
-            const { statusCode } = res
+            const {
+                statusCode
+            } = res
 
             let error
             if (statusCode !== 200) error = new Error(`Request Failed.\n'Status Code: ${statusCode}`)
@@ -57,13 +61,68 @@ const obj2URL = function (obj, encode) {
     return strAry.join('&')
 }
 
-const getIP = function () {
+const getCurrentIP = function () {
     return httpGet('ident.me')
+}
+
+const dateFormat = function (date = new Date(), format) {
+    var str = format || 'YYYY-MM-DD hh:mm:ss'
+    var Week = ['日', '一', '二', '三', '四', '五', '六']
+
+    str = str.replace(/yyyy|YYYY/, date.getFullYear())
+    str = str.replace(/yy|YY/, (date.getYear() % 100) > 9 ? (date.getYear() % 100).toString() : '0' + (date.getYear() % 100))
+    str = str.replace(/MM/, date.getMonth() > 9 ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1))
+    str = str.replace(/M/g, date.getMonth())
+    str = str.replace(/w|W/g, Week[date.getDay()])
+
+    str = str.replace(/dd|DD/, date.getDate() > 9 ? date.getDate().toString() : '0' + date.getDate())
+    str = str.replace(/d|D/g, date.getDate())
+
+    str = str.replace(/hh|HH/, date.getHours() > 9 ? date.getHours().toString() : '0' + date.getHours())
+    str = str.replace(/h|H/g, date.getHours())
+    str = str.replace(/mm/, date.getMinutes() > 9 ? date.getMinutes().toString() : '0' + date.getMinutes())
+    str = str.replace(/m/g, date.getMinutes())
+
+    str = str.replace(/ss|SS/, date.getSeconds() > 9 ? date.getSeconds().toString() : '0' + date.getSeconds())
+    str = str.replace(/s|S/g, date.getSeconds())
+
+    return str
+}
+
+const log = function (iMsg) {
+
+    let date = dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')
+
+    let file_path = path.resolve(__dirname, '..', 'ip_log.txt')
+    console.log(file_path)
+    fs.open(file_path, 'a', function (err, fd) {
+
+        let msg = date + ' => ' + iMsg + '\n'
+
+        //同步写入
+        fs.writeSync(fd, msg);
+    });
+}
+
+const getLastLog = function () {
+
+    let data = '', lineNum = 0
+    let file_path = path.resolve(__dirname, '..', 'ip_log.txt')
+    let isExists = fs.existsSync(file_path)
+
+    // 若不存在
+    if (!isExists) return null
+
+    data = fs.readFileSync(file_path, 'utf-8').split('\n')
+    lineNum = data.length
+    return data[lineNum - 2]
 }
 
 module.exports = {
     httpGet,
     HmacSHA1,
     obj2URL,
-    getIP
+    getCurrentIP,
+    log,
+    getLastLog
 }
