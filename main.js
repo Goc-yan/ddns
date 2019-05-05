6195a177f27417d278e9d8ac672440a1997343ed
Goc-yan <632114989@qq.com>
2019-05-05 11:22:41 +0800

    simple config

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { AccessKeyId, AccessKeySecret, RecordId } = require('./config/user')
+const { AccessKeyId, AccessKeySecret, DomainName } = require('./config/user')
 
 const AliCloudClient = require('./aliCloudClient')
 const { getCurrentIP, dateFormat, getLastLog } = require('./utils')
@@ -6,16 +6,15 @@ const { getCurrentIP, dateFormat, getLastLog } = require('./utils')
 let main = async function () {
 
     try {
+
         // 历史IP
         let preIPRecord = await getLastLog()
         let preIP = preIPRecord ? preIPRecord.split(' => ')[1] : ''
         // 当前IP
         let currentIP = await getCurrentIP()
         
+        // 若 IP 未变, 不执行改 IP 操作
         if (preIP === currentIP) return
-        
-        let currentTime = dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')
-        console.log(currentTime, '=>', currentIP)
 
         // 更新 DomainRecord
         let UpdateDomainRecord = function (RecordId, IP) {
@@ -28,7 +27,21 @@ let main = async function () {
                 RecordId,
             }
             return aliCloudClient.get(options).then(res => {
-                console.log(currentIP, '=> update success')
+                let currentTime = dateFormat(new Date(), 'YYYY-MM-DD hh:mm:ss')
+                console.log(currentTime, '=>', currentIP)
+            })
+        }
+
+        // 获取 RecordId
+        let getRecordId = function () {
+
+            let options = {
+                Action: 'DescribeDomainRecords',
+                DomainName
+            }
+            return aliCloudClient.get(options).then(res => {
+                let Record = res.DomainRecords.Record
+                return Array.isArray(Record) ? Record[0].RecordId : Record.RecordId
             })
         }
 
@@ -36,6 +49,8 @@ let main = async function () {
             AccessKeyId,
             AccessKeySecret,
         })
+  
+        let RecordId = await getRecordId()
 
         // 更新域映射IP
         UpdateDomainRecord(RecordId, currentIP)
